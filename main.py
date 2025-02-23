7477f2ee09c0c159b2b55ced4e839b3be3f3c58f
Brandon <bgtall96@gmail.com>
2025-02-23 08:34:02 -0800

    created player and asteroids objects, got them successfully moving on screen

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield_1 = AsteroidField()
+
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\n"
@@ -14,7 +33,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        for i in drawable:
+            i.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
 
